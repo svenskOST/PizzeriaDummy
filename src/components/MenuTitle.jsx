216ea483543b0a85ddb229e8a2e76bd712da5cb3
svenskOST <svenskost6@gmail.com>
2023-11-13 00:37:32 +0100

    små bug fixar

--- src/components/MenuTitle.jsx
+++ src/components/MenuTitle.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 function MenuTitle({ title }) {
    return (
-      <h1 className='mx-auto mb-10 mt-36 w-2/3 min-w-fit rounded-2xl bg-green-800 px-16 text-center text-8xl text-gray-100 lg:ml-[5vw] lg:rounded-3xl lg:text-9xl'>
+      <h1 className='mx-auto mb-5 md:mb-10 mt-28 md:mt-36 w-fit rounded-2xl bg-green-800 px-10 md:px-16 text-center text-7xl md:text-8xl text-gray-100 lg:ml-[5vw] lg:rounded-3xl lg:text-9xl'>
          {title}
       </h1>
    )
