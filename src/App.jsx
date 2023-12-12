1f094fb113cb0aa4792281d0b7c8230fd1812d31
svenskOST <svenskost6@gmail.com>
2023-12-12 15:47:01 +0100

    structure small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import { useState } from 'react'
-import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Menu from './pages/Menu'
 import About from './pages/About'
