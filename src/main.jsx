0d64d52030d34f319eb201e4d6fac3f568ce278f
Alexander Marini <svenskost6@gmail.com>
2024-02-17 02:13:01 +0100

    fixed comment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      {/*ändra basename inför produktion, t.ex. '/app-och-webb'*/}
+      {/*ändra basename inför produktion, t.ex. '/app-och-webb/arskars-3/pizzeria-dummy'*/}
       <BrowserRouter basename=''>
          <App />
       </BrowserRouter>
